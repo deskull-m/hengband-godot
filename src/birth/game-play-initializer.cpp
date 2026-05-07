Hourier/hengband

[Refactor] town-records.cpp/h を定義してPlayerType::visit を廃止した

src/birth/game-play-initializer.cpp
@@ -22,6 +22,7 @@
 #include "system/enums/dungeon/dungeon-id.h"
 #include "system/floor/floor-info.h"
 #include "system/floor/floor-list.h"
+#include "system/floor/town-records.h"
 #include "system/floor/wilderness-grid.h"
 #include "system/inner-game-data.h"
 #include "system/item-entity.h"
@@ -114,7 +115,7 @@ void player_wipe_without_name(PlayerType *player_ptr)
     player_ptr->pet_follow_distance = PET_FOLLOW_DIST;
     player_ptr->pet_extra_flags = (PF_TELEPORT | PF_ATTACK_SPELL | PF_SUMMON_SPELL);
     DungeonRecords::get_instance().reset_all();
-    player_ptr->visit = 1;
+    TownRecords::get_instance().reset_all();
     world.set_wild_mode(false);
     WildernessGrids::get_instance().initialize_position();
 

src/load/load-zangband.cpp
@@ -23,6 +23,7 @@
 #include "system/dungeon/dungeon-record.h"
 #include "system/enums/dungeon/dungeon-id.h"
 #include "system/floor/floor-info.h"
+#include "system/floor/town-records.h"
 #include "system/inner-game-data.h"
 #include "system/monrace/monrace-definition.h"
 #include "system/monrace/monrace-list.h"
@@ -194,10 +195,10 @@ void set_zangband_action(PlayerType *player_ptr)
     }
 }
 
-void set_zangband_visited_towns(PlayerType *player_ptr)
+void set_zangband_visited_towns()
 {
     strip_bytes(4);
-    player_ptr->visit = 1L;
+    TownRecords::get_instance().reset_all();
 }
 
 void set_zangband_quest(PlayerType *player_ptr, QuestType *const q_ptr, const QuestId loading_quest_index, const QuestId old_inside_quest)

src/system/floor/town-records.cpp
@@ -1,0 +1,44 @@
+#include "system/floor/town-records.h"
+#include "system/floor/town-list.h"
+#include "util/enum-converter.h"
+
+TownRecords TownRecords::instance{};
+
+TownRecords &TownRecords::get_instance()
+{
+    return instance;
+}
+
+bool TownRecords::has_visited(TownId id) const
+{
+    if (enum2i(id) == SECRET_TOWN) {
+        return false;
+    }
+
+    return this->visited_ids.has(id);
+}
+
+void TownRecords::set_visited(TownId id)
+{
+    if (enum2i(id) == SECRET_TOWN) {
+        return;
+    }
+
+    this->visited_ids.set(id);
+}
+
+void TownRecords::reset_all()
+{
+    this->visited_ids.clear();
+    this->visited_ids.set(TownId::OUTPOST);
+}
+
+void TownRecords::set_ids(const EnumClassFlagGroup<TownId> &loaded_data)
+{
+    this->visited_ids = loaded_data;
+}
+
+EnumClassFlagGroup<TownId> TownRecords::get_ids() const
+{
+    return this->visited_ids;
+}

deskull-m/hengband-godot

src/birth/game-play-initializer.cpp
@@ -22,6 +22,7 @@
 #include "system/enums/dungeon/dungeon-id.h"
 #include "system/floor/floor-info.h"
 #include "system/floor/floor-list.h"
+#include "system/floor/town-records.h"
 #include "system/floor/wilderness-grid.h"
 #include "system/inner-game-data.h"
 #include "system/item-entity.h"
@@ -114,7 +115,7 @@ void player_wipe_without_name(PlayerType *player_ptr)
     player_ptr->pet_follow_distance = PET_FOLLOW_DIST;
     player_ptr->pet_extra_flags = (PF_TELEPORT | PF_ATTACK_SPELL | PF_SUMMON_SPELL);
     DungeonRecords::get_instance().reset_all();
-    player_ptr->visit = 1;
+    TownRecords::get_instance().initialize();
     world.set_wild_mode(false);
     WildernessGrids::get_instance().initialize_position();
 

src/load/load-zangband.cpp
@@ -23,6 +23,7 @@
 #include "system/dungeon/dungeon-record.h"
 #include "system/enums/dungeon/dungeon-id.h"
 #include "system/floor/floor-info.h"
+#include "system/floor/town-records.h"
 #include "system/inner-game-data.h"
 #include "system/monrace/monrace-definition.h"
 #include "system/monrace/monrace-list.h"
@@ -194,10 +195,10 @@ void set_zangband_action(PlayerType *player_ptr)
     }
 }
 
-void set_zangband_visited_towns(PlayerType *player_ptr)
+void set_zangband_visited_towns()
 {
     strip_bytes(4);
-    player_ptr->visit = 1L;
+    TownRecords::get_instance().initialize();
 }
 
 void set_zangband_quest(PlayerType *player_ptr, QuestType *const q_ptr, const QuestId loading_quest_index, const QuestId old_inside_quest)

src/system/floor/town-records.cpp
@@ -1,0 +1,44 @@
+#include "system/floor/town-records.h"
+#include "system/floor/town-list.h"
+#include "util/enum-converter.h"
+
+TownRecords TownRecords::instance{};
+
+TownRecords &TownRecords::get_instance()
+{
+    return instance;
+}
+
+bool TownRecords::has_visited(TownId id) const
+{
+    if (enum2i(id) == SECRET_TOWN) {
+        return false;
+    }
+
+    return this->visited_ids.has(id);
+}
+
+void TownRecords::set_visited(TownId id)
+{
+    if (enum2i(id) == SECRET_TOWN) {
+        return;
+    }
+
+    this->visited_ids.set(id);
+}
+
+void TownRecords::initialize()
+{
+    this->visited_ids.clear();
+    this->visited_ids.set(TownId::OUTPOST);
+}
+
+void TownRecords::set_ids(const EnumClassFlagGroup<TownId> &loaded_data)
+{
+    this->visited_ids = loaded_data;
+}
+
+EnumClassFlagGroup<TownId> TownRecords::get_ids() const
+{
+    return this->visited_ids;
+}