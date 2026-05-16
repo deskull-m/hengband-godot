Hourier/hengband

[Refactor] ItemEntity::ident をビットフラグからEnumClassFlagGroup に差し替えた
併せて2点変更：
- フラグの残りが少ないので8ビットから16ビットに拡張した
- セーブデータ形式が変わるのでバージョンを25にアップした

src/artifact/fixed-art-generator.cpp
@@ -16,7 +16,6 @@
 #include "floor/floor-object.h"
 #include "object-enchant/object-boost.h"
 #include "object-enchant/object-curse.h"
-#include "object-enchant/special-object-flags.h"
 #include "player-base/player-class.h"
 #include "specific-object/bloody-moon.h"
 #include "system/artifact-type-definition.h"
@@ -181,7 +180,7 @@ static void fixed_artifact_random_abilities(PlayerType *player_ptr, const Artifa
 static void invest_curse_to_fixed_artifact(const ArtifactType &artifact, ItemEntity *o_ptr)
 {
     if (!artifact.cost) {
-        set_bits(o_ptr->ident, IDENT_BROKEN);
+        o_ptr->set_special_flag(SpecialItemFlag::BROKEN);
     }
 
     if (artifact.gen_flags.has(ItemGenerationTraitType::CURSED)) {

src/effect/effect-monster.cpp
@@ -33,7 +33,6 @@
 #include "monster/monster-status.h"
 #include "monster/monster-update.h"
 #include "monster/monster-util.h"
-#include "object-enchant/special-object-flags.h"
 #include "spell-kind/blood-curse.h"
 #include "spell-kind/spells-polymorph.h"
 #include "spell-kind/spells-teleport.h"
@@ -688,7 +687,7 @@ static void postprocess_by_taking_photo(PlayerType *player_ptr, EffectMonster *e
 
     ItemEntity item({ ItemKindType::STATUE, SV_PHOTO });
     item.pval = em_ptr->photo;
-    item.ident |= (IDENT_FULL_KNOWN);
+    item.set_special_flag(SpecialItemFlag::FULL_KNOWN);
     (void)drop_near(player_ptr, item, player_ptr->get_position());
 }
 

src/mind/mind-mage.cpp
@@ -10,7 +10,6 @@
 #include "flavor/object-flavor-types.h"
 #include "floor/floor-object.h"
 #include "inventory/inventory-object.h"
-#include "object-enchant/special-object-flags.h"
 #include "object-hook/hook-magic.h"
 #include "object/item-tester-hooker.h"
 #include "object/item-use-flags.h"
@@ -80,7 +79,7 @@ bool eat_magic(PlayerType *player_ptr, int power)
             }
 
             if (!item->pval) {
-                item->ident |= IDENT_EMPTY;
+                item->set_special_flag(SpecialItemFlag::EMPTY);
             }
         }
     }

src/object-enchant/item-magic-applier.cpp
@@ -10,7 +10,6 @@
 #include "object-enchant/enchanter-factory.h"
 #include "object-enchant/item-apply-magic.h"
 #include "object-enchant/object-curse.h"
-#include "object-enchant/special-object-flags.h"
 #include "player/player-status-flags.h"
 #include "system/artifact-type-definition.h"
 #include "system/baseitem/baseitem-definition.h"
@@ -205,7 +204,7 @@ void ItemMagicApplier::apply_cursed()
     }
 
     if (this->o_ptr->is_worthless()) {
-        set_bits(this->o_ptr->ident, IDENT_BROKEN);
+        this->o_ptr->set_special_flag(SpecialItemFlag::BROKEN);
     }
 
     const auto &baseitem = this->o_ptr->get_baseitem();

src/spell-kind/magic-item-recharger.cpp
@@ -16,7 +16,6 @@
 #include "flavor/object-flavor-types.h"
 #include "floor/floor-object.h"
 #include "inventory/inventory-object.h"
-#include "object-enchant/special-object-flags.h"
 #include "object-hook/hook-magic.h"
 #include "object/item-tester-hooker.h"
 #include "object/item-use-flags.h"
@@ -107,8 +106,7 @@ bool recharge(PlayerType *player_ptr, int power)
             }
 
             item->pval += recharge_amount;
-            item->ident &= ~(IDENT_KNOWN);
-            item->ident &= ~(IDENT_EMPTY);
+            item->reset_special_flags({ SpecialItemFlag::KNOWN, SpecialItemFlag::EMPTY });
         }
     }
 

src/view/display-characteristic.cpp
@@ -9,7 +9,6 @@
 #include "view/display-characteristic.h"
 #include "flavor/flavor-util.h"
 #include "inventory/inventory-slot-types.h"
-#include "object-enchant/special-object-flags.h"
 #include "object-enchant/tr-types.h"
 #include "object-enchant/trc-types.h"
 #include "perception/object-perception.h"
@@ -109,7 +108,7 @@ static void process_cursed_equipment_characteristics(PlayerType *player_ptr, uin
     for (int i = INVEN_MAIN_HAND; i < max_i; i++) {
         auto *o_ptr = player_ptr->inventory[i].get();
         auto is_known = o_ptr->is_known();
-        auto is_sensed = is_known || o_ptr->ident & IDENT_SENSE;
+        auto is_sensed = is_known || o_ptr->has_special_flag(SpecialItemFlag::SENSE);
         auto flags = o_ptr->get_flags_known();
 
         if (flags.has(TR_ADD_L_CURSE) || flags.has(TR_ADD_H_CURSE)) {

deskull-m/hengband-godot

src/artifact/fixed-art-generator.cpp
@@ -16,7 +16,6 @@
 #include "floor/floor-object.h"
 #include "object-enchant/object-boost.h"
 #include "object-enchant/object-curse.h"
-#include "object-enchant/special-object-flags.h"
 #include "player-base/player-class.h"
 #include "specific-object/bloody-moon.h"
 #include "system/artifact/artifact-definition.h"
@@ -183,7 +182,7 @@ static void fixed_artifact_random_abilities(PlayerType *player_ptr, const Artifa
 static void invest_curse_to_fixed_artifact(const ArtifactDefinition &artifact, ItemEntity *o_ptr)
 {
     if (!artifact.cost) {
-        set_bits(o_ptr->ident, IDENT_BROKEN);
+        o_ptr->set_identification_flag(IdentificationFlag::BROKEN);
     }
 
     if (artifact.gen_flags.has(ItemGenerationTraitType::CURSED)) {

src/effect/effect-monster.cpp
@@ -33,7 +33,6 @@
 #include "monster/monster-status.h"
 #include "monster/monster-update.h"
 #include "monster/monster-util.h"
-#include "object-enchant/special-object-flags.h"
 #include "spell-kind/blood-curse.h"
 #include "spell-kind/spells-polymorph.h"
 #include "spell-kind/spells-teleport.h"
@@ -688,7 +687,7 @@ static void postprocess_by_taking_photo(PlayerType *player_ptr, EffectMonster *e
 
     ItemEntity item({ ItemKindType::STATUE, SV_PHOTO });
     item.pval = em_ptr->photo;
-    item.ident |= (IDENT_FULL_KNOWN);
+    item.set_identification_flag(IdentificationFlag::FULL_KNOWN);
     (void)drop_near(player_ptr, item, player_ptr->get_position());
 }
 

src/mind/mind-mage.cpp
@@ -10,7 +10,6 @@
 #include "flavor/object-flavor-types.h"
 #include "floor/floor-object.h"
 #include "inventory/inventory-object.h"
-#include "object-enchant/special-object-flags.h"
 #include "object-hook/hook-magic.h"
 #include "object/item-tester-hooker.h"
 #include "object/item-use-flags.h"
@@ -80,7 +79,7 @@ bool eat_magic(PlayerType *player_ptr, int power)
             }
 
             if (!item->pval) {
-                item->ident |= IDENT_EMPTY;
+                item->set_identification_flag(IdentificationFlag::EMPTY);
             }
         }
     }

src/object-enchant/item-magic-applier.cpp
@@ -10,7 +10,6 @@
 #include "object-enchant/enchanter-factory.h"
 #include "object-enchant/item-apply-magic.h"
 #include "object-enchant/object-curse.h"
-#include "object-enchant/special-object-flags.h"
 #include "player/player-status-flags.h"
 #include "system/baseitem/baseitem-definition.h"
 #include "system/dungeon/dungeon-definition.h"
@@ -204,7 +203,7 @@ void ItemMagicApplier::apply_cursed()
     }
 
     if (this->o_ptr->is_worthless()) {
-        set_bits(this->o_ptr->ident, IDENT_BROKEN);
+        this->o_ptr->set_identification_flag(IdentificationFlag::BROKEN);
     }
 
     const auto &baseitem = this->o_ptr->get_baseitem();

src/spell-kind/magic-item-recharger.cpp
@@ -16,7 +16,6 @@
 #include "flavor/object-flavor-types.h"
 #include "floor/floor-object.h"
 #include "inventory/inventory-object.h"
-#include "object-enchant/special-object-flags.h"
 #include "object-hook/hook-magic.h"
 #include "object/item-tester-hooker.h"
 #include "object/item-use-flags.h"
@@ -107,8 +106,7 @@ bool recharge(PlayerType *player_ptr, int power)
             }
 
             item->pval += recharge_amount;
-            item->ident &= ~(IDENT_KNOWN);
-            item->ident &= ~(IDENT_EMPTY);
+            item->reset_identification_flags({ IdentificationFlag::KNOWN, IdentificationFlag::EMPTY });
         }
     }
 

src/view/display-characteristic.cpp
@@ -9,7 +9,6 @@
 #include "view/display-characteristic.h"
 #include "flavor/flavor-util.h"
 #include "inventory/inventory-slot-types.h"
-#include "object-enchant/special-object-flags.h"
 #include "object-enchant/tr-types.h"
 #include "object-enchant/trc-types.h"
 #include "perception/object-perception.h"
@@ -109,7 +108,7 @@ static void process_cursed_equipment_characteristics(PlayerType *player_ptr, uin
     for (int i = INVEN_MAIN_HAND; i < max_i; i++) {
         auto *o_ptr = player_ptr->inventory[i].get();
         auto is_known = o_ptr->is_known();
-        auto is_sensed = is_known || o_ptr->ident & IDENT_SENSE;
+        auto is_sensed = is_known || o_ptr->has_identification_flag(IdentificationFlag::SENSE);
         auto flags = o_ptr->get_flags_known();
 
         if (flags.has(TR_ADD_L_CURSE) || flags.has(TR_ADD_H_CURSE)) {