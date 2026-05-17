Hourier/hengband

[Refactor] AutopickMethodのenum classを作った

src/autopick/autopick.cpp
@@ -105,7 +105,7 @@ void autopick_pickup_items(PlayerType *player_ptr, const Grid &grid)
         auto &item = *player_ptr->current_floor_ptr->o_list[this_o_idx];
         int idx = find_autopick_list(player_ptr, &item);
         auto_inscribe_item(&item, idx);
-        if ((idx < 0) || (autopick_list[idx].action & (DO_AUTOPICK | DO_QUERY_AUTOPICK)) == 0) {
+        if ((idx < 0) || (autopick_list[idx].action.has_all_of({ AutopickMethod::AUTOPICK, AutopickMethod::QUERY_AUTOPICK }))) {
             auto_destroy_item(player_ptr, &item, idx);
             continue;
         }
@@ -118,7 +118,7 @@ void autopick_pickup_items(PlayerType *player_ptr, const Grid &grid)
             continue;
         }
 
-        if (!(autopick_list[idx].action & DO_QUERY_AUTOPICK)) {
+        if (!(autopick_list[idx].action.has(AutopickMethod::QUERY_AUTOPICK))) {
             process_player_pickup_item(player_ptr, this_o_idx);
             continue;
         }

src/cmd-visual/cmd-map.cpp
@@ -20,7 +20,7 @@ void do_cmd_view_map(PlayerType *player_ptr)
     prt(_("お待ち下さい...", "Please wait..."), 0, 0);
     term_fresh();
     term_clear();
-    display_autopick = 0;
+    display_autopick.clear();
 
     int cy, cx;
     display_map(player_ptr, &cy, &cx);
@@ -35,7 +35,7 @@ void do_cmd_view_map(PlayerType *player_ptr)
         return;
     }
 
-    display_autopick = ITEM_DISPLAY;
+    display_autopick.set(AutopickMethod::DISPLAY);
     while (true) {
         const auto &[wid, hgt] = term_get_size();
         int row_message = hgt - 1;
@@ -44,29 +44,29 @@ void do_cmd_view_map(PlayerType *player_ptr)
             row_message, 1);
         move_cursor(cy, cx);
         int i = inkey(true);
-        byte flag;
+        EnumClassFlagGroup<AutopickMethod> flags;
         if ('M' == i) {
-            flag = (DO_AUTOPICK | DO_QUERY_AUTOPICK);
+            flags.set({ AutopickMethod::AUTOPICK, AutopickMethod::QUERY_AUTOPICK });
         } else if ('N' == i) {
-            flag = DONT_AUTOPICK;
+            flags.set(AutopickMethod::AUTOPICK);
         } else if ('K' == i) {
-            flag = DO_AUTODESTROY;
+            flags.set(AutopickMethod::AUTODESTROY);
         } else if ('D' == i) {
-            flag = (DO_AUTOPICK | DO_QUERY_AUTOPICK | DONT_AUTOPICK);
+            flags.set({ AutopickMethod::AUTOPICK, AutopickMethod::QUERY_AUTOPICK, AutopickMethod::NOT_AUTOPICK });
         } else {
             break;
         }
 
         term_fresh();
-        if (~display_autopick & flag) {
-            display_autopick |= flag;
+        if (display_autopick.has_all_of(flags)) {
+            display_autopick.reset(flags);
         } else {
-            display_autopick &= ~flag;
+            display_autopick.set(flags);
         }
 
         display_map(player_ptr, &cy, &cx);
     }
 
-    display_autopick = 0;
+    display_autopick.clear();
     screen_load();
 }

src/knowledge/knowledge-autopick.cpp
@@ -48,18 +48,17 @@ void do_cmd_knowledge_autopick(PlayerType *player_ptr)
 
     for (const auto &entry : autopick_list) {
         std::string command;
-        const auto act = entry.action;
-        if (any_bits(act, DONT_AUTOPICK)) {
+        if (entry.action.has(AutopickMethod::AUTOPICK)) {
             command = _("放置", "Leave");
-        } else if (act & DO_AUTODESTROY) {
+        } else if (entry.action.has(AutopickMethod::AUTODESTROY)) {
             command = _("破壊", "Destroy");
-        } else if (act & DO_AUTOPICK) {
+        } else if (entry.action.has(AutopickMethod::AUTOPICK)) {
             command = _("拾う", "Pickup");
         } else {
             command = _("確認", "Query");
         }
 
-        const auto fmt = any_bits(act, DO_DISPLAY) ? "[%s]" : "(%s)";
+        const auto fmt = entry.action.has(AutopickMethod::DISPLAY) ? "[%s]" : "(%s)";
         fprintf(fff, "%11s", format(fmt, command.data()).data());
         const auto line = autopick_line_from_entry(entry);
         fprintf(fff, " %s", line.data());

deskull-m/hengband-godot

src/autopick/autopick.cpp
@@ -105,7 +105,7 @@ void autopick_pickup_items(PlayerType *player_ptr, const Grid &grid)
         auto &item = *player_ptr->current_floor_ptr->o_list[this_o_idx];
         int idx = find_autopick_list(player_ptr, &item);
         auto_inscribe_item(&item, idx);
-        if ((idx < 0) || (autopick_list[idx].action & (DO_AUTOPICK | DO_QUERY_AUTOPICK)) == 0) {
+        if ((idx < 0) || (autopick_list[idx].action.has_none_of({ AutopickMethod::AUTOPICK, AutopickMethod::QUERY_AUTOPICK }))) {
             auto_destroy_item(player_ptr, &item, idx);
             continue;
         }
@@ -118,7 +118,7 @@ void autopick_pickup_items(PlayerType *player_ptr, const Grid &grid)
             continue;
         }
 
-        if (!(autopick_list[idx].action & DO_QUERY_AUTOPICK)) {
+        if (!(autopick_list[idx].action.has(AutopickMethod::QUERY_AUTOPICK))) {
             process_player_pickup_item(player_ptr, this_o_idx);
             continue;
         }

src/cmd-visual/cmd-map.cpp
@@ -20,7 +20,7 @@ void do_cmd_view_map(PlayerType *player_ptr)
     prt(_("お待ち下さい...", "Please wait..."), 0, 0);
     term_fresh();
     term_clear();
-    display_autopick = 0;
+    display_autopick.clear();
 
     int cy, cx;
     display_map(player_ptr, &cy, &cx);
@@ -35,7 +35,7 @@ void do_cmd_view_map(PlayerType *player_ptr)
         return;
     }
 
-    display_autopick = ITEM_DISPLAY;
+    display_autopick.set(AutopickMethod::ITEM_DISPLAY);
     while (true) {
         const auto &[wid, hgt] = term_get_size();
         int row_message = hgt - 1;
@@ -44,29 +44,29 @@ void do_cmd_view_map(PlayerType *player_ptr)
             row_message, 1);
         move_cursor(cy, cx);
         int i = inkey(true);
-        byte flag;
+        EnumClassFlagGroup<AutopickMethod> flags;
         if ('M' == i) {
-            flag = (DO_AUTOPICK | DO_QUERY_AUTOPICK);
+            flags.set({ AutopickMethod::AUTOPICK, AutopickMethod::QUERY_AUTOPICK });
         } else if ('N' == i) {
-            flag = DONT_AUTOPICK;
+            flags.set(AutopickMethod::NOT_AUTOPICK);
         } else if ('K' == i) {
-            flag = DO_AUTODESTROY;
+            flags.set(AutopickMethod::AUTODESTROY);
         } else if ('D' == i) {
-            flag = (DO_AUTOPICK | DO_QUERY_AUTOPICK | DONT_AUTOPICK);
+            flags.set({ AutopickMethod::AUTOPICK, AutopickMethod::QUERY_AUTOPICK, AutopickMethod::NOT_AUTOPICK });
         } else {
             break;
         }
 
         term_fresh();
-        if (~display_autopick & flag) {
-            display_autopick |= flag;
+        if (display_autopick.has_all_of(flags)) {
+            display_autopick.reset(flags);
         } else {
-            display_autopick &= ~flag;
+            display_autopick.set(flags);
         }
 
         display_map(player_ptr, &cy, &cx);
     }
 
-    display_autopick = 0;
+    display_autopick.clear();
     screen_load();
 }

src/knowledge/knowledge-autopick.cpp
@@ -48,18 +48,17 @@ void do_cmd_knowledge_autopick(PlayerType *player_ptr)
 
     for (const auto &entry : autopick_list) {
         std::string command;
-        const auto act = entry.action;
-        if (any_bits(act, DONT_AUTOPICK)) {
+        if (entry.action.has(AutopickMethod::NOT_AUTOPICK)) {
             command = _("放置", "Leave");
-        } else if (act & DO_AUTODESTROY) {
+        } else if (entry.action.has(AutopickMethod::AUTODESTROY)) {
             command = _("破壊", "Destroy");
-        } else if (act & DO_AUTOPICK) {
+        } else if (entry.action.has(AutopickMethod::AUTOPICK)) {
             command = _("拾う", "Pickup");
         } else {
             command = _("確認", "Query");
         }
 
-        const auto fmt = any_bits(act, DO_DISPLAY) ? "[%s]" : "(%s)";
+        const auto fmt = entry.action.has(AutopickMethod::DISPLAY) ? "[%s]" : "(%s)";
         fprintf(fff, "%11s", format(fmt, command.data()).data());
         const auto line = autopick_line_from_entry(entry);
         fprintf(fff, " %s", line.data());